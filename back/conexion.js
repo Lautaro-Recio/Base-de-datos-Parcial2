78343ad7697b216990c4b17c78398b44bd8039d6
FacuAudagna <facundithor@gmail.com>
2025-11-14 10:11:39 -0300

    fix conexion

--- back/conexion.js
+++ back/conexion.js
@@ -4,13 +4,14 @@ import { MongoClient } from 'mongodb';
 dotenv.config();
 
 const uri = process.env.URI;
+const dbName = process.env.DB_NAME;
 
 async function conectar() {
     try {
         console.log("✅ Conectado a MongoDB Atlas");
         const cliente = new MongoClient(uri)
         await cliente.connect()
-        const db = cliente.db(process.env.DB_NAME);
+        const db = cliente.db(dbName);
         return db;
     } catch (error) {
         console.log("❌ Error al conectar con MongoDB:", error);
